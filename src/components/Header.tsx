6537ad57eb2dd67acbae4b65306fd5fbb3798be3
nohseongbong <shtjdqhd@naver.com>
2023-12-18 21:06:30 +0900

    design : header 작업중

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
+import styles from '@styles/header/header.module.css';
+
 export default function Header() {
   return (
-    <header className="fixed inset-x-0 top-0 z-50 left-0 bg-white text-gray-700 body-font border-b border-gray-200">
-      <div className="container flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <header className={styles.header}>
+      {/* <div className="container flex flex-wrap p-5 flex-col md:flex-row items-center">
         <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <span className="ml-3 text-xl text-indigo-500">NKE</span>
         </div>
@@ -14,7 +16,7 @@ export default function Header() {
             연락방법
           </div>
         </nav>
-      </div>
+      </div> */}
     </header>
   );
 }
